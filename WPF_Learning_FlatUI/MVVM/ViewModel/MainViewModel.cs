fff1ad049d821567e6220aba71e6f450c98ffa07
Frederic <paulpoiu123@gmail.com>
2022-01-02 13:22:33 +0100

    Finished.

--- WPF_Learning_FlatUI/MVVM/ViewModel/MainViewModel.cs
+++ WPF_Learning_FlatUI/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,40 @@ namespace WPF_Learning_FlatUI.MVVM.ViewModel
 {
     class MainViewModel : ObservbleObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVm { get; set; }
+        public DiscoveryViewModel DiscoveryVm { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
+            HomeVm = new HomeViewModel();
+            DiscoveryVm = new DiscoveryViewModel();
 
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVm;
+            });
         }
     }
 }
